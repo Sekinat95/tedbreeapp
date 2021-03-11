6dd2b4cb44944b49d25d7c856db77b580bf56fff
sekinat95 <sekinat95@gmail.com>
2021-03-11 18:20:52 +0100

    updated readme file

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## About My implemeentation
 
-Please use both postman as well as the .http file for testin:
+Please use both postman as well as the .http file for testing:
+business login url:
+
 POST http://localhost:8000/V1/login
 Content-Type: application/json
 
@@ -9,7 +11,7 @@ Content-Type: application/json
 "password": "password"
 }
 
-###
+guest login url:
 
 POST http://localhost:8000/V1/guest/login
 Content-Type: application/json
@@ -19,4 +21,9 @@ Content-Type: application/json
 "password": "password"
 }
 
-## Learning Laravel
+## Other information
+
+I have answered all questions save for the following two:
+
+-   guest post an application for a job
+-   business sees all applications for each of their jobs
--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -1,10 +1,66 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Application;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class ApplicationController extends Controller
 {
-    //
+    protected $guest;
+    
+    public function __construct(){
+       $this->guest = $this->guard('guest')->guest();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function apply(Request $request, $job_id)
+    {
+        $validator = Validator::make($request->all(), [ 
+            'firstname' => 'required|string', 
+            'lastname' => 'required|string', 
+            'email' => 'required|string', 
+            'phone' => 'required|string',
+            'location' => 'required|string',
+            'cv' => 'required|string',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        $application = new Application();
+        $application ->firstname = $request->firstname;
+        $application ->lastname = $request->lastname;
+        $application ->email = $request->email;
+        $application ->phone = $request->phone;
+        $application ->location = $request->location;
+        $application ->cv = $request->cv;
+
+        if($this->guest->applications()->save($application)){
+            return response()->json([
+                'status'=>true,
+                'application'=>$application 
+
+            ]);
+        }else{
+            return response()->json([
+                'status'=>false,
+                'message'=>'new job could not be saved'
+            ]);
+        }
+    }//end apply
+
+    protected function guard(){
+        return Auth::guard();
+    }
 }
